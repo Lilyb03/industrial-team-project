a5e71098e247c927040971b252d209a1565e0b30
Lily <91525799+Lilyb03@users.noreply.github.com>
2024-09-23 20:56:32 +0100

    Created React Project

---
+++ malarcays-web/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+//import './App.css'
+//import './Tailwind.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <head>
+        <title>Malarcays Demo</title>
+      </head>
+      <body>
+        <h1>Hello, world!</h1>
+      </body>
+    </>
+  )
+}
+
+export default App
