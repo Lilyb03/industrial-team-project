acf4b5d6b81ca9f64655219b924364c504db1e02
lilyb03 <2451786@dundee.ac.uk>
2024-09-24 17:45:48 +0100

    Added navbar and buttons

--- malarcays-web/src/App.tsx
+++ malarcays-web/src/App.tsx
@@ -1,19 +1,47 @@
-import { useState } from 'react'
 //import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <head>
-        <title>Malarcays Demo</title>
-      </head>
+      <header>
+      <Navbar className="bg-body-secondary">
+          <Container>
+            <Col>
+              T
+            </Col>
+
+            <Col>
+              <Row>Hugh Mann</Row>
+            </Col>
+            
+            <Col>
+            Green Level: 1
+            </Col>
+          </Container>
+        </Navbar>
+      </header>
       <body>
-        <h1>Hello, world!</h1>
+      
       </body>
+      <footer>
+          <Navbar className="bg-body-secondary fixed-bottom">
+              <Container>
+                  <Button variant="secondary">Profile</Button>
+                  <Button variant="secondary">Activity</Button>
+                  <Button variant="secondary">Settings</Button>
+                  <Button variant="secondary">More</Button>
+              </Container>
+          </Navbar>
+      </footer>
     </>
   )
 }
