2fa997bcd307b2dc45f046dfbde908417267e40a
lilyb03 <2451786@dundee.ac.uk>
2024-09-25 15:18:56 +0100

    addded page switching functionality

--- malarcays-web/src/App.tsx
+++ malarcays-web/src/App.tsx
@@ -33,31 +33,73 @@ function TopBar(){
   )
 }
 
-function BottomBar(){
+/**
+ *  TODO: figure out how to type switchPage so that TS doesn't give errors
+ */
+function BottomBar({setPage}: {setPage: (pageNumber: number) => void}) {
   return (
   <>
     <Navbar className="bg-body-secondary fixed-bottom">
       <Container>
-        <Button variant="secondary">Profile</Button>
-        <Button variant="secondary">Activity</Button>
-        <Button variant="secondary">Settings</Button>
-        <Button variant="secondary">More</Button>
+        <Button variant="secondary" onClick={() => setPage(0)}>Profile</Button>
+        <Button variant="secondary" onClick={() => setPage(1)}>Activity</Button>
+        <Button variant="secondary" onClick={() => setPage(2)}>Settings</Button>
+        <Button variant="secondary" onClick={() => setPage(3)}>More</Button>
       </Container>
     </Navbar>
   </>
   ) 
 }
 
+function MainPage({page}: {page: number}){
+
+  switch(page) {
+    case 0:
+      return (
+        <>
+        <h1>first page</h1>
+        </>
+      )
+    break;
+    case 1:
+      return (
+        <>
+        <h1>second page</h1>
+        </>
+      )
+    break;
+    case 2:
+      return (
+        <>
+        <h1>third page</h1>
+        </>
+      )
+    break;
+    case 3:
+      return (
+        <>
+        <h1>fourth page</h1>
+        </>
+      )
+    break;
+  }
+  
+}
+
 function App() {
+
+  const [page, setPage] = useState(0);
+
   return (
     <>
       <header>
         <TopBar />
       </header>
       <body>
+        <MainPage page={page} />
       </body>
       <footer>
-        <BottomBar />
+        <BottomBar setPage={setPage}/>
       </footer>
     </>
   )
