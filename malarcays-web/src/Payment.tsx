c657aaa3afc66087ff77db21b4de5ae0d3c08c6e
Kelly the Snail <91427256+EvansKelly92@users.noreply.github.com>
2024-09-26 16:45:29 +0100

    adds ui framework for payment confirmation

--- malarcays-web/src/Payment.tsx
+++ malarcays-web/src/Payment.tsx
@@ -26,6 +26,7 @@ function Payment(){
         else
         {
           //magic happens here it should probaby replace stuff beg Lily on how to actually change the content
+          //see below for the content that should replace the form's content
         }
     
       }
@@ -62,3 +63,32 @@ function Payment(){
 }
 
 export default Payment
+
+//This is so bad I'm sorry. For some reason the multi line comment isn't working
+
+  //        <Stack gap={2}>
+  //            {/* should send user back to main */}
+   //           <Button className='m-3' id='backbutt' variant="light"><Image src='/img/back.svg'></Image></Button>
+   //           <h2>Send Payment</h2>
+   //           <Container className='ml-3 mr-3 d-grid p-3' id='box'>
+     //           {/* should be entry.amt, if the float doesnt have .00, .00 should be added */}
+       //        <h1>£100.00</h1>
+         //      {/*should be entry.payee */}
+           //    <h4><em>To:</em> <strong>CompanyName</strong></h4>
+             //  {/*This is the spot that shows the transaction number stored on the database*/}
+               //<h4><em>Ref:</em> <strong>1234567890</strong></h4>
+//              </Container>
+  //            <br/>
+    //          <Container>
+      //        <h2>Paying With</h2>
+        //      </Container>
+          //    <Container className='ml-3 mr-3 d-grid p-3' id='box'>
+            //    {/*users account number here*/}
+              //  <h4><em>Account:</em> <strong>C123467890</strong></h4>
+                //{/*account number here*/}
+//                <h4><em>Current Amount:</em> <strong>£10000.00</strong></h4>
+  //              <h4><em>After Payment:</em><strong>£{paymentResult}</strong></h4>
+    //          </Container>
+      //        {/*why isnt this centered, I cry. anyways. on click it should do the database calls and adjusting */}
+        //      <Button className='mt-3' id='butt' varaint='primary'>Send Payment</Button>
+      //</Stack>
