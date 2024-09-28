68d1a15fcc2686e7ae35f907238926336e7c378a
GeorghiosTziouliou <2412649@dundee.ac.uk>
2024-09-28 15:32:50 +0100

    Added comments to files

--- malarcays-api/src/accounts/accounts.service.ts
+++ malarcays-api/src/accounts/accounts.service.ts
@@ -5,26 +5,36 @@ import { CreateAccountsDTO } from './dtos/createAccounts.dto';
 @Injectable()
 export class AccountsService {
   async createAccount(createAccountsDto: CreateAccountsDTO) {
+    //function to create an account
+
     const { name, lastName, type_id, spending_category, amount } = createAccountsDto;
 
+
+    //Get all the details ids from details table
     const allDetailsIds = await sql`
         SELECT details_id FROM details;
       `;
 
-    const allType = await sql`
-        SELECT * FROM type;
-      `;
+    // const allType = await sql`
+    //     SELECT * FROM type;
+    //   `;
 
+    //get all account Ids fdrom account table
     const allAccountIds = await sql`
         SELECT account_number FROM account;
       `;
 
-    const accountId = allAccountIds.length + 1;
 
+    //find the highest and add +1 so we know the index where we will insert the new data
+    const accountId = allAccountIds.length + 1;
     const detailsId = allDetailsIds.length + 1;
 
 
-    const detailsResult = await sql`
+    /*Insert the details we got from
+    * @param {CreateAccountsDTO} body
+    * into the dielnd table
+    */
+    await sql`
       INSERT INTO details (details_id, name, last_name)
       VALUES (${detailsId}, ${name}, ${lastName || null}) 
     `;
@@ -32,22 +42,24 @@ export class AccountsService {
     let createdAccount;
 
 
+    //We check the type of the account we want to create
     if (type_id === 'COMPANY') {
 
+      //Again we get all company ids to prevent duplication and add +1 to the length
       const allCompanyIds = await sql`
         SELECT company_id FROM company;
       `;
-
       const companyId = allCompanyIds.length + 1;
 
-      const companyResult = await sql`
+      //Insert the data from the dto into company table
+      await sql`
         INSERT INTO company (company_id, details_id, spending_category)
         VALUES (${companyId}, ${detailsId}, ${spending_category})
         RETURNING company_id;
       `;
 
-      // const companyId = companyResult[0].company_id;
 
+      //and finally create the account from data from dto if its a company
       createdAccount = await sql`
         INSERT INTO account (account_number, details_id, type_id, amount, company_id)
         VALUES (${accountId}, ${detailsId}, '2', ${amount}, ${companyId})
@@ -55,8 +67,7 @@ export class AccountsService {
       `;
     } else {
 
-
-
+      //create a customer account if its not a company
       createdAccount = await sql`
         INSERT INTO account (account_number, details_id, type_id, amount)
         VALUES (${accountId}, ${detailsId}, '1', ${amount})
@@ -64,24 +75,29 @@ export class AccountsService {
       `;
     }
 
+    //return the created account
     return createdAccount;
   }
 
   async login(name: string) {
     try {
-
+      /*quarry to get/fetch account data
+      and procide by joining the account and details table
+      then selects everything from accounts and details
+      where the name matches
+      */
       const rows = await sql`
         SELECT * FROM account a
         JOIN details d ON a.details_id = d.details_id
         WHERE d.name = ${name}
       `;
-
+      //if nothing was found return null
       if (rows.length === 0) {
         return null;
       }
-
       return rows[0];
     } catch (error) {
+      //return the error if an error occured
       throw new Error(`Error fetching account: ${error.message}`);
     }
   }
