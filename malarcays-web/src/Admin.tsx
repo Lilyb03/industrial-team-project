f9d48ce6eda96d720dd443d1f47e09336e5f696d
uhgh123 <72364284+uhgh123@users.noreply.github.com>
2024-10-03 18:39:34 +0100

    initial admin page setup

---
+++ malarcays-web/src/Admin.tsx
@@ -0,0 +1,7 @@
+import { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { AccountData } from './services/api';
