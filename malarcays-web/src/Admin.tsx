fbb986f9417e7fc76d8c9d1ea653640a28aa2f16
uhgh123 <72364284+uhgh123@users.noreply.github.com>
2024-10-03 18:43:26 +0100

    API fetch
    fetching user and company data

--- malarcays-web/src/Admin.tsx
+++ malarcays-web/src/Admin.tsx
@@ -5,3 +5,17 @@ import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { AccountData } from './services/api';
+
+export function getUserByAccount(accountNumber: string) {
+  const usersURL = `https://api.malarcays.uk/search/account?account=${accountNumber}`;
+  return fetch(usersURL, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((res) => res.json())
+    .catch((error) => {
+      console.error('Error fetching users:', error);
+    });
+}
